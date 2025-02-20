10b6c3c2fb3baccdb717cff5425983608bbc7eaf
nikolay dementiev <nikolay@medisafe.com>
2025-02-20 18:02:46 +0100

    create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeed
+//
+//  Created by Mykola Dementiev
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
