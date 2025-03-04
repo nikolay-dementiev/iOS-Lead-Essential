c9036bc5653df4969c55a66e0e2b3b8f661364c3
nikolay dementiev <nikolas.dementiev@gmail.com>
2025-03-04 12:16:46 +0100

    guaranti we only recive one error

--- EssentialFeed/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -39,13 +39,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT(url: url)
         
         client.error = NSError(domain: "Test", code: 0)
-        var capturedError: RemoteFeedLoader.Error?
-        sut.load { error in
-            capturedError = error
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load {
+            capturedErrors.append($0)
         }
         
-        XCTAssertEqual(capturedError,
-                       .connectivity)
+        XCTAssertEqual(capturedErrors,
+                       [.connectivity])
     }
 
     // MARK: Helpers
