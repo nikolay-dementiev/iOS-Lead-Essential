3172bc347a1ce563a09ed0ba3a7869bc708d72c4
nikolay dementiev <nikolas.dementiev@gmail.com>
2025-03-17 19:31:56 +0100

    remove `LoadFeedResult` Equatable conformance from production codesince it's test only requirements

--- EssentialFeed/EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -129,22 +129,27 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private func expect(_ sut: RemoteFeedLoader,
-                        toCompleteWithResult result: RemoteFeedLoader.Result,
+                        toCompleteWithResult expectedResult: RemoteFeedLoader.Result,
                         when action: () -> Void,
                         file: StaticString = #filePath,
                         line: UInt = #line) {
         
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load {
-            capturedResults.append($0)
+        let exp = expectation(description: "Wait for load completion.")
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected \(expectedResult), but got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
         }
         
         action()
-        
-        XCTAssertEqual(capturedResults,
-                       [result],
-                       file: file,
-                       line: line)
+        wait(for: [exp], timeout: 0.3)
     }
     
     func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
