760eb65a1d01f1ab2d075e8c098332fb4e76f0d1
nikolay dementiev <nikolas.dementiev@gmail.com>
2025-03-17 15:41:57 +0100

    move HTTPClient to its own file

---
+++ EssentialFeed/EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeed
+//
+//  Created by Mykola Dementiev
+//
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
