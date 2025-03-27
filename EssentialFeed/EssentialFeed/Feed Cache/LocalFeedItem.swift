db09d339c6ebb2681edc1ed87bfaaecd11b52531
nikolay dementiev <nikolas.dementiev@gmail.com>
2025-03-27 18:48:59 +0100

    move `LocalFeedItem` to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  EssentialFeed
+//
+//  Created by Mykola Dementiev
+//
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID,
+                description: String? = nil,
+                location: String? = nil,
+                imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
