e82f04fd3a8514b07925147298f1a5cbed9be708
nikolay dementiev <nikolas.dementiev@gmail.com>
2025-04-01 16:38:17 +0200

    move `feed cachepolicy` to its own file

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -2,26 +2,7 @@
 //  EssentialFeed
 //
 //  Created by Mykola Dementiev
-//
-
-
-private final class FeedCachePolicy {
-    private static let calendar = Calendar(identifier: .gregorian)
-    
-    private static var maxCacheAgeInDays: Int {
-        7
-    }
-    
-    private init() {}
-    
-    static func validate(_ timeStamp: Date, agains date: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timeStamp) else {
-            return false
-        }
-        
-        return date < maxCacheAge
-    }
-}
+// 
 
 public final class LocalFeedLoader {
     private let store: FeedStore
