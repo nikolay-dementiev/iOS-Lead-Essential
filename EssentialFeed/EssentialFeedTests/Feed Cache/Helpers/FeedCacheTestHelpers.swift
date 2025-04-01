54e5dff750811809816ed1a3d2896c189990bf30
nikolay dementiev <nikolas.dementiev@gmail.com>
2025-04-01 17:06:31 +0200

    sepatare date extension helpers into distinct contexts to clarify thir scope

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -34,7 +34,9 @@ extension Date {
     private func adding(days: Int) -> Date {
         Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
-    
+}
+
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
     }
