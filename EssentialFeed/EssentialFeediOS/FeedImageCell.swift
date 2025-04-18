6fb78b41fe89d3a5665e65cf9285719dc50ed6f2
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-04-18 11:34:21 +0200

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeed
+//
+//  Created by Mykola Dementiev
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+}
