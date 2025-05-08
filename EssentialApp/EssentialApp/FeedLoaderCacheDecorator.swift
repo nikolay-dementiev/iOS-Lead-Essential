28e2d9052cb76c8d7cf08c5b4f225fd319123029
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-05-08 14:12:03 +0200

    Move FeedLoaderCacheDecorator to production

---
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,28 @@
+//
+//  EssentialApp
+//
+//  Created by Mykola Dementiev
+//
+
+import EssentialFeed
+
+public final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            guard let self else { return }
+            
+            completion(result.map { feed in
+                self.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
