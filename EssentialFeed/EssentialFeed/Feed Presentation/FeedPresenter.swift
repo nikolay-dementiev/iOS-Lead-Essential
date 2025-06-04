a1f72bdbb4147827fa5656eec7c441d6a0932ede
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-06-04 15:52:41 +0200

    Add image comments title

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -11,15 +11,8 @@ public final class FeedPresenter {
     public static var title: String {
         NSLocalizedString("FEED_VIEW_TITLE",
                           tableName: "Feed",
-                          bundle: Bundle(for: FeedPresenter.self),
+                          bundle: Bundle(for: Self.self),
                           comment: "Title for the Feed view")
-    }
-    
-    private var feedLoadError: String {
-        NSLocalizedString("GENERIC_CONNECTION_ERROR",
-                          tableName: "Shared",
-                          bundle: Bundle(for: FeedPresenter.self),
-                          comment: "Error message displayed when we can't load the image feed from the server")
     }
     
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeed
+//
+//  Created by Mykola Dementiev
+//
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+    
+    public static var title: String {
+        NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+                          tableName: "ImageComments",
+                          bundle: Bundle(for: Self.self),
+                          comment: "Title for the Image Comments View")
+    }
+}
