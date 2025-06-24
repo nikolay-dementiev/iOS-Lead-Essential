46575b62268ac72dc978e45a6ace557b29200097
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-06-24 16:09:11 +0200

    Dispatches from background to main queue

--- EssentialApp/EssentialAppTests/Comments UI/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/Comments UI/CommentsUIIntegrationTests.swift
@@ -95,7 +95,7 @@ final class CommentsUIIntegrationTests: FeedUIIntegrationTests {
 //        RunLoop.current.run(until: Date()+1)
     }
     
-    override func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+    func test_loadCommentsCompletion_dispatchesFromBackgroundToMainThread() {
         let (sut, loader) = makeSUT()
         sut.simulateAppearance()
         
